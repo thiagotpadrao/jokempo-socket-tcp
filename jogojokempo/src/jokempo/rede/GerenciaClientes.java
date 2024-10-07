838760dba9c0cb2fa154ef69422df3f94756677e
Felipe Bavuso <Felipe Bavuso@DESKTOP-3D4VME1>
2024-10-06 21:13:53 -0300

    Acréscimo e atualização de mensagens.

--- jogojokempo/src/jokempo/rede/GerenciaClientes.java
+++ jogojokempo/src/jokempo/rede/GerenciaClientes.java
@@ -12,16 +12,33 @@ public class GerenciaClientes implements Runnable{
 	private Socket clienteSocket;
 	private BufferedReader in;
 	private PrintWriter out;
-	
+	private int playerid;
 	private static Jogo jogo = new Jogo();
 	private static Jogada jogadaplayer1;
 	private static Jogada jogadaplayer2;
+	private static PrintWriter msgplayer1;
+	private static PrintWriter msgplayer2;
 	
 	public GerenciaClientes (Socket socket) {
 		this.clienteSocket = socket;
 		try {
 			in = new BufferedReader(new InputStreamReader(clienteSocket.getInputStream()));
             out = new PrintWriter(clienteSocket.getOutputStream(), true);
+            
+            synchronized (jogo) {
+                if (msgplayer1 == null) {
+                	msgplayer1 = out;
+                	playerid = 1;
+                	msgplayer1.println("\nAguardando um oponente se conectar para começar...");
+                } else if (msgplayer2 == null) {
+                	msgplayer2 = out;
+                	playerid = 2;
+                	msgplayer2.println("\nSeu adversário já estava te esperando. O jogo vai começar!");
+                	msgplayer1.println("Oponente conectado. O jogo vai começar!");
+                	
+                	solicitarJogada();
+                }
+            }
 		}catch (IOException e) {
             System.err.println("Erro ao configurar streams: " + e.getMessage());
         }
@@ -35,24 +52,33 @@ public class GerenciaClientes implements Runnable{
                     Jogada jogadaplayer = Jogada.valueOf(input.toUpperCase());
 
                     synchronized (jogo) {
-                        if (jogadaplayer1 == null) {
+                        if (playerid == 1 && jogadaplayer1 == null) {
                         	jogadaplayer1 = jogadaplayer;
-                            out.println("Aguardando jogada do oponente...");
-                        } else if (jogadaplayer2 == null) {
+                            if (jogadaplayer2 == null) msgplayer1.println("Aguardando jogada do oponente...");
+                            if (msgplayer2 != null) {
+                                if (jogadaplayer2 == null) msgplayer2.println("Seu adversário jogou.");
+                            }
+                        } else if (playerid == 2 && jogadaplayer2 == null) {
+                        	jogadaplayer2 = jogadaplayer;
+                            if(jogadaplayer1 == null) msgplayer2.println("Aguardando jogada do oponente...");
+                            if (msgplayer1 != null) {
+                            	if (jogadaplayer1 == null) msgplayer1.println("Seu adversário jogou.");
+                            }
+                        }
+                        
+                        if (jogadaplayer1 != null && jogadaplayer2 != null) {
                         	jogadaplayer2 = jogadaplayer;
                         	
                         	Rodada resultadoplayer1 = jogo.jogar(jogadaplayer1, jogadaplayer2);
+                        	Rodada resultadoplayer2 = (resultadoplayer1 == Rodada.VITORIA) ? Rodada.DERROTA : (resultadoplayer1 == Rodada.DERROTA ? Rodada.VITORIA : Rodada.EMPATE);
                         	
-                        	if (resultadoplayer1 == Rodada.VITORIA) {
-                                out.println("Você venceu a rodada!");
-                            } else if (resultadoplayer1 == Rodada.DERROTA) {
-                                out.println("Você perdeu a rodada!");
-                            } else {
-                                out.println("A rodada foi empate!");
-                            }
-                        	
-                        	jogadaplayer1 = null;
-                        	jogadaplayer2 = null;
+                            enviaResultado(msgplayer1, resultadoplayer1);
+                            enviaResultado(msgplayer2, resultadoplayer2);
+                            
+                            jogadaplayer1 = null;
+                            jogadaplayer2 = null;
+                            
+                            solicitarJogada();
                         }
                     }
                 } catch (IllegalArgumentException e) {
@@ -70,6 +96,27 @@ public class GerenciaClientes implements Runnable{
             }
 		}     
 	}
+	
+	private void enviaResultado(PrintWriter msgplayer, Rodada resultado) {
+    	if (resultado == Rodada.VITORIA) {
+            msgplayer.println("Você venceu a rodada!");
+        } else if (resultado == Rodada.DERROTA) {
+            msgplayer.println("Você perdeu a rodada!");
+        } else {
+            msgplayer.println("A rodada foi empate!");
+        }
+	}
+	
+    private void solicitarJogada() {
+        if (msgplayer1 != null) {
+        	msgplayer1.println("\n--------------------Começo de nova rodada--------------------\n");
+        	msgplayer1.println("Insira sua jogada (PEDRA, PAPEL ou TESOURA): ");
+        }
+        if (msgplayer2 != null) {
+        	msgplayer2.println("\n--------------------Começo de nova rodada--------------------\n");
+        	msgplayer2.println("Insira sua jogada (PEDRA, PAPEL ou TESOURA): ");
+        }
+    }
 	
 	public void sendMessage(String message) {
 		out.println(message);
